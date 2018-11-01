a85e9ccb836e532188cc54cfd861eac445ebe003
Nolan Kovacik <noltron000@gmail.com>
2018-11-01 15:38:20 -0700

    changing game variables up

--- script.js
+++ script.js
@@ -2,31 +2,39 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
+// Canvas Variables
+let MarginTop = 80;
+let MarginSide = 80;
+
 // Ball Variables
-let ballRadius = 10;
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-let dx = 3;
-let dy = -3;
+let ballRadius = 128;
+let x = (canvas.width - MarginSide * 2 - ballRadius * 2) * Math.random() + MarginSide + ballRadius;
+let y = (canvas.height / 3) * Math.random() + canvas.height / 2;
+let dx = 4;
+let dy = -4;
+let max_dx = 24;
+let max_dy = 24;
 
 // Paddle Variables
-let paddleHeight = 10;
-let paddleWidth = 75;
-let paddleRaise = paddleHeight;
+let paddleHeight = 15;
+let paddleWidth = 288;
+let min_pWidth = paddleWidth / 24
+let paddleRaise = paddleHeight / 2;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
 
 // Brick Variables
-let brickRowCount = 8;
-let brickColumnCount = 12;
-let brickPadding = 2;
-let brickOffsetTop = 30;
-let brickOffsetLeft = 30;
-let brickWidth = (canvas.width - brickOffsetLeft * 2) / brickColumnCount;
-let brickHeight = (canvas.height - brickOffsetTop) / (brickRowCount * 4);
+let brickRowCount = 16;
+let brickColCount = 32;
+let brickPadding = 0;
+let brickHeight = (canvas.height / 2 - MarginTop) / (brickRowCount);
+let brickWidth = (canvas.width - MarginSide * 2 - (brickPadding * (brickRowCount - 1) / brickColCount)) / brickColCount;
+
+
+
 let bricks = [];
-for (let c = 0; c < brickColumnCount; c++) {
+for (let c = 0; c < brickColCount; c++) {
 	bricks[c] = [];
 	for (let r = 0; r < brickRowCount; r++) {
 		bricks[c][r] = { x: 0, y: 0, status: true };
@@ -35,7 +43,7 @@ for (let c = 0; c < brickColumnCount; c++) {
 
 // Defining UI Variables
 let score = 0;
-let lives = 3;
+let lives = 9;
 let game_on = true;
 
 // Defining COLOR Variables
@@ -85,13 +93,14 @@ const getRandomColor = () => {
 }
 
 const rainbowMode = (color) => {
-	console.log(color)
+	if (color > 360) {
+		color = color - 360
+	}
 	return `hsl(${color}, 100%, 50%)`
-
 }
 
 const collisionDetection = () => {
-	for (let c = 0; c < brickColumnCount; c++) {
+	for (let c = 0; c < brickColCount; c++) {
 		for (let r = 0; r < brickRowCount; r++) {
 			let b = bricks[c][r];
 			if (b.status == true) {
@@ -107,9 +116,15 @@ const collisionDetection = () => {
 					// Regardless, Continue on with function
 					b.status = false;
 					score++;
-					dy = dy * 25 / 24;
-					dx = dx * 25 / 24;
-					if (score == brickRowCount * brickColumnCount) {
+					dy = dy + ((max_dy - Math.abs(dy)) * Math.abs(dy) / (dy * max_dy ** 2))
+					dx = dx + ((max_dx - Math.abs(dx)) * Math.abs(dx) / (dx * max_dx ** 2))
+					ballRadius += 20 / ballRadius;
+					console.log(dy)
+					console.log(Math.abs(dy) - Math.abs(dx))
+					paddleWidth = paddleWidth - ((min_pWidth - paddleWidth) / (min_pWidth ** 2))
+					paddleX += paddleWidth * 1 / 128
+					paddleWidth = paddleWidth * (95 / 96);
+					if (score == brickRowCount * brickColCount) {
 						alert("YOU WIN, CONGRATULATIONS!");
 						document.location.reload();
 					}
@@ -120,16 +135,27 @@ const collisionDetection = () => {
 }
 
 const drawBall = () => {
-	ctx.fillStyle = rainbowMode(ballColor);
-	ballColor += 1
+	// left edge of paddle, 0, left of paddle + width of paddle, 0
+	let clr = ctx.createLinearGradient(x, y, x + ballRadius, y + ballRadius);
+	clr.addColorStop(0, rainbowMode(ballColor)); // Places a color at the start
+	clr.addColorStop(1, rainbowMode(ballColor + 30)); // Places a color at the end
+	ballColor += 2
+
+	// Fill with gradient
+	ctx.fillStyle = clr;
+
+	// Create gradient
 	ctx.beginPath();
 	ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
 	ctx.fill();
+	ctx.stroke();
 	ctx.closePath();
 }
 
 const drawPaddle = () => {
-	ctx.fillStyle = 'gray'
+	// Fill with gradient
+	ctx.fillStyle = 'grey';
+
 	ctx.beginPath();
 	ctx.rect(paddleX, canvas.height - paddleHeight - paddleRaise, paddleWidth, paddleHeight);
 	ctx.fill();
@@ -138,19 +164,19 @@ const drawPaddle = () => {
 
 const drawBricks = () => {
 	ctx.fillStyle = 'gray'
-	for (let c = 0; c < brickColumnCount; c++) {
+	for (let c = 0; c < brickColCount; c++) {
 		for (let r = 0; r < brickRowCount; r++) {
 			if (bricks[c][r].status == true) {
-				let brickX = (c * (brickWidth + brickPadding * 2)) + brickOffsetLeft;
-				let brickY = (r * (brickHeight + brickPadding * 2)) + brickOffsetTop;
+				let brickX = (c * (brickWidth + brickPadding * 2));
+				let brickY = (r * (brickHeight + brickPadding * 2));
 				bricks[c][r].x = brickX;
 				bricks[c][r].y = brickY;
 				if ((r % 2) == 0) {
-					brickX = (c * (brickWidth + brickPadding)) + brickWidth / 2 + brickOffsetLeft;
-					brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+					brickX = (c * (brickWidth + brickPadding)) + MarginSide + brickWidth / 4;
+					brickY = (r * (brickHeight + brickPadding)) + MarginTop;
 				} else {
-					brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-					brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+					brickX = (c * (brickWidth + brickPadding)) + MarginSide - brickWidth / 4;
+					brickY = (r * (brickHeight + brickPadding)) + MarginTop;
 				}
 				bricks[c][r].x = brickX;
 				bricks[c][r].y = brickY;
@@ -202,15 +228,14 @@ const draw = () => {
 		if (!lives) {
 			if (game_on) {
 				game_on = false
-				alert("GAME OVER");
+				alert("GAME OVER"); // YOU LOSE
 				document.location.reload();
 			}
 		}
 		else {
-			x = canvas.width / 2;
-			y = canvas.height - 30;
+			x = (canvas.width - MarginSide * 2 - ballRadius * 2) * Math.random() + MarginSide + ballRadius;
+			y = (canvas.height / 3) * Math.random() + canvas.height / 2;
 			dy = -Math.abs(dy);
-			paddleX = (canvas.width - paddleWidth) / 2;
 		}
 	}
 
@@ -222,6 +247,7 @@ const draw = () => {
 	}
 	x += dx;
 	y += dy;
+	ballRadius /= 1 + 1 / 128;
 	requestAnimationFrame(draw);
 }
 
