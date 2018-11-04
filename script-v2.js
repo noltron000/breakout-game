dfdd03f6a2af8914bfc23716603496383bc46907
Nolan Kovacik <noltron000@gmail.com>
2018-11-03 20:58:07 -0700

    move function being distributed among subclasses

--- script-v2.js
+++ script-v2.js
@@ -1,4 +1,5 @@
 canvas = document.getElementById("myCanvas");
+canvas.length = canvas.width
 ctx = canvas.getContext("2d"); // CANVAS CONTEXT
 
 class Node { // PARENT NODE — SETS UP INTERACTABLE SHAPES //
@@ -10,32 +11,7 @@ class Node { // PARENT NODE — SETS UP INTERACTABLE SHAPES //
 		this.colour = colour;
 	}
 
-	draw() { // CATCH-ALL DRAW FUNCTION //
-		ctx.fillStyle = this.colour
-		ctx.beginPath();
-		if ((this.length != undefined) && (this.height != undefined)) { // This is a square.
-			ctx.rect(this.x, this.y, this.length, this.height);
-		}
-		else if (this.radius != undefined) { // This object has a radius. It is a circle.
-			ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-		}
-		else { // This object is neither square nor circle. Thus, it is henceforth a dot.
-			ctx.rect(this.x, this.y, 1, 1)
-		}
-		ctx.fill();
-		ctx.closePath();
-	}
-
-	move() { // CATCH-ALL MOVE FUNCTION //
-		console.log(canvas.width)
-		console.log(this.length)
-		if (this.pressR == true) {
-			this.x += 7;
-		}
-		else if (this.pressL == true) {
-			this.x -= 7;
-		}
-
+	boundary() { // LIMITS MOVEMENT TO CANVAS //
 		if (this.x < 0) { // too far left
 			this.x = 0;
 		}
@@ -43,8 +19,26 @@ class Node { // PARENT NODE — SETS UP INTERACTABLE SHAPES //
 			this.x = canvas.width - this.length;
 		}
 
+		if (this.y < 0) { // too far up
+			this.y = 0;
+		}
+		else if (this.y > canvas.height - this.height) { // too far down
+			this.y = canvas.height - this.height;
+		}
+	}
+
+	draw() { // PIXEL NODE
+		ctx.fillStyle = this.colour
+		ctx.beginPath();
+		ctx.rect(this.x, this.y, 1, 1)
+		ctx.fill();
+		ctx.closePath();
+	}
+
+	move() { // MOVE OBJECT
 		this.x += this.xDelta
 		this.y += this.yDelta
+		this.boundary()
 	}
 }
 
@@ -52,6 +46,14 @@ class Ball extends Node { // CREATES GAME BALL WITH PHYSICS //
 	constructor(x, y, xDelta, yDelta, radius, colour) {
 		super(x, y, xDelta, yDelta, colour);
 		this.radius = radius;
+	}
+
+	draw() {
+		ctx.fillStyle = this.colour
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+		ctx.fill();
+		ctx.closePath();
 	}
 }
 
@@ -69,6 +71,27 @@ class Paddle extends Node { // CREATES USER CONTROLLED PADDLE //
 		document.addEventListener("keydown", this.keyDownHandler.bind(this));
 		document.addEventListener("keyup", this.keyUpHandler.bind(this));
 		document.addEventListener("mousemove", this.mouseMoveHandler.bind(this));
+	}
+
+
+	draw() {
+		ctx.fillStyle = this.colour
+		ctx.beginPath();
+		ctx.rect(this.x, this.y, this.length, this.height);
+		ctx.fill();
+		ctx.closePath();
+	}
+
+	move() {
+		if (this.pressR == true) {
+			this.x += 7;
+		}
+		else if (this.pressL == true) {
+			this.x -= 7;
+		}
+		this.x += this.xDelta
+		this.y += this.yDelta
+		this.boundary()
 	}
 
 	keyDownHandler(event) { //
@@ -101,25 +124,21 @@ class Brick extends Node { // CREATES DESTRUCTABLE BRICKS //
 		this.height = height;
 		this.health = health;
 	}
+
+	draw() {
+		ctx.fillStyle = this.colour
+		ctx.beginPath();
+		ctx.rect(this.x, this.y, this.length, this.height);
+		ctx.fill();
+		ctx.closePath();
+	}
 }
-
-/*
-HUD CLASS
-*/
-
-	/*
-	LIVES CLASS
-	*/
-
-	/*
-	SCORES CLASS
-	*/
 
 class Game { // GAME CLASS //
 	constructor() {
 		this.ball = new Ball(50, 50, 0, 0, 15, 'purple')
 		this.brick = new Brick(349, 12, 0, 0, 94, 53, 'red', 1)
-		this.paddle = new Paddle(70, 70, 0, 0, 50, 75, 'blue', 'a', 'a')
+		this.paddle = new Paddle(120, canvas.height - 20, 0, 0, 120, 15, 'blue', 'a', 'a')
 	}
 
 	loop() { // ADDS THE ILLUSION OF MOTION OVER TIME //
@@ -144,3 +163,19 @@ class Game { // GAME CLASS //
 
 const game = new Game()
 game.loop()
+
+
+
+
+
+/*
+HUD CLASS
+*/
+
+	/*
+	LIVES CLASS
+	*/
+
+	/*
+	SCORES CLASS
+	*/
