8ddad15956e049fe02984bec643bb83c58764d1c
Nolan <noltron000@gmail.com>
2021-08-10 20:43:33 -0400

    Adding basic collision calculator to rectangleMOB

--- src/classes/rectangle-mob.js
+++ src/classes/rectangle-mob.js
@@ -7,6 +7,72 @@ class RectangleMOB extends MobileObject {
 		this.color = color
 	}
 
+	get length () {
+		return this.dimensions[0]
+	}
+
+	get height () {
+		return this.dimensions[1]
+	}
+
+	get xPos () {
+		return this.coordinates[0][0]
+	}
+
+	get left () {
+		return this.xPos
+	}
+
+	get right () {
+		return this.xPos + this.length
+	}
+
+	get yPos () {
+		return this.coordinates[0][1]
+	}
+
+	get up () {
+		return this.yPos
+	}
+
+	get down () {
+		return this.yPos + this.height
+	}
+
+	// This diagram might help...
+	// O<=>O   X<->X ❌️
+	// X<->X   O<=>O ❌️
+	// O<=<X>=>O-->X ✔️
+	// X<-<O<=<X>=>O ✔️
+	// O<=<X<->X>=>O ✔️
+	// X<-<O<=>O>->X ✔️
+	// ...Where X and O are this and that.
+	// Arrows connect between left and right Xs and Os.
+
+	sharesDomainWith (that) {
+		return (
+			this.left < that.right
+		) && (
+			that.left < this.right
+		)
+	}
+
+	sharesRangeWith (that) {
+		return (
+			this.up < that.down
+		) && (
+			that.up < this.down
+		)
+	}
+
+	sharesSpaceWith (that) {
+		return (
+			this.sharesDomainWith(that)
+		) && (
+			this.sharesRangeWith(that)
+		)
+	}
+
 	draw () {
 		this.coordinates = this.nextFrame
 		const context = this.canvasContext
