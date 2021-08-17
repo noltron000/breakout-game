9106b64046d776dd6eac9f145ff19aaa1dd542b0
Nolan <noltron000@gmail.com>
2021-08-17 19:01:45 -0400

    Ball now has health

--- src/classes/ball.js
+++ src/classes/ball.js
@@ -3,6 +3,7 @@ import RectangleMOB from './rectangle-mob.js'
 class Ball extends RectangleMOB {
 	constructor (canvasContext, {coordinates, dimensions}) {
 		super(canvasContext, {coordinates, dimensions, color: 'white'})
+		this.health = 1
 	}
 }
 
